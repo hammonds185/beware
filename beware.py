1ba60f66d8320b1e88dba025a25f08b7f31936a8
Courtney Hammonds <hammcourt20@gmail.com>
2022-07-28 13:20:53 +0000

    multipage template

--- beware.py
+++ beware.py
@@ -3,8 +3,28 @@ from flask import Flask, render_template, abort, request, flash
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def home():
     return render_template('home.html') 
+
+@app.route("/crimemap")
+def crime_map():
+    return render_template('crimemap.html') 
+
+@app.route("/bewaremap")
+def beware_map():
+    return render_template('bewaremap.html') 
+
+@app.route("/login")
+def login():
+    return render_template('login.html') 
+
+@app.route("/myprofile")
+def profile():
+    return render_template('profile.html') 
+
+@app.route("/report")
+def report():
+    return render_template('report.html')
 
 if __name__ == '__main__':
     # configure()
