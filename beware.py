58b3e4f38d60e305d78ecb52423f9392de0bbde2
Ian Kamweru <ikamweru24@amherst.edu>
2022-07-29 20:14:52 +0000

    login and registration page

--- beware.py
+++ beware.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, abort, request, flash
+from flask import Flask, render_template, redirect, url_for, abort, request, flash, session
 from dotenv import load_dotenv
+from __init__ import app, db, bcrypt
+from forms import *
+from model import *
 import os
-
-app = Flask(__name__)
 
 def configure():
     load_dotenv()
@@ -15,9 +16,38 @@ def home():
 def beware_map():
     return render_template('bewaremap.html', latitude = 0, longitude = 0, url = "https://maps.googleapis.com/maps/api/js?key=" + os.getenv('API_KEY') + "&callback=initMap") 
 
-@app.route("/login")
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html') 
+    form = LoginForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        email=form.email.data
+        password=form.password.data
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            flash(f'Logging you in', 'success')
+            session['username'] = user.username
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    global user
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checks if entries are valid
+        pwd_hash = bcrypt.generate_password_hash(form.password.data).decode("utf-8")   
+        user = User(username = form.username.data, email = form.email.data, password = pwd_hash)
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except:
+            flash('username or email taken, try again')
+            return render_template('register.html', title='Register', form=form)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 @app.route("/myprofile")
 def profile():
