057258eb5e89a3c0d8becbcf6ea5cc278cb1421c
Amana28 <76058143+Amana28@users.noreply.github.com>
2022-07-29 15:50:31 -0400

    added an input field

--- beware.py
+++ beware.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, abort, request, flash
 from dotenv import load_dotenv
+import requests
 import os
 
 app = Flask(__name__)
@@ -12,8 +13,28 @@ def home():
     return render_template('home.html')  
 
 @app.route("/bewaremap")
+def first_map():
+    # default map is NY
+    return render_template('bewaremap.html', latitude = 40.7128, longitude = 74.0060, url = "https://maps.googleapis.com/maps/api/js?key=" + os.getenv('API_KEY') + "&callback=initMap")
+
+@app.route("/bewaremap", methods=['POST', 'GET'])
 def beware_map():
-    return render_template('bewaremap.html', latitude = 0, longitude = 0, url = "https://maps.googleapis.com/maps/api/js?key=" + os.getenv('API_KEY') + "&callback=initMap") 
+    address = request.form["location"]
+    params = {
+        'key': os.getenv('API_KEY'),
+        'address': address
+    }
+    base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    if data['status'] == 'OK':
+        result = data['results'][0]
+        location = result['geometry']['location']
+        LAT = location['lat']
+        LNG = location['lng']
+    else:
+        return "address is invalid"
+    return render_template('bewaremap.html', latitude = LAT, longitude = LNG, url = "https://maps.googleapis.com/maps/api/js?key=" + os.getenv('API_KEY') + "&callback=initMap") 
 
 @app.route("/login")
 def login():
